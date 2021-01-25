dd0061f2794636008e64e7d4a22e4ab72372d775
Merlin <merlinzhao@gmail.com>
2021-01-24 20:49:13 -0500

    first commit

---
+++ CovidTracker/CovidTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackerApp.swift
+//  CovidTracker
+//
+//  Created by Merlin Zhao on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
